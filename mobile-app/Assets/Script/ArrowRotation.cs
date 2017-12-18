8e7ed74e3f848011b2931293b32d1a56e7abb341
Supanut Rongluang-aram <supanut2feb2539@hotmail.com>
2017-12-19 00:16:42 +0700

    Initial commit

---
+++ mobile-app/Assets/Script/ArrowRotation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowRotation : MonoBehaviour {
+	public GameObject obj;
+	public int maxWait = 10;
+	private int i = 0;
+	private LocationService service;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (getLocation ());
+	}
+
+	IEnumerator getLocation(){
+		service = Input.location;
+		if (!service.isEnabledByUser) {
+			Debug.Log("Location Services not enabled by user");
+			yield break;
+		}
+		service.Start();
+		while (service.status == LocationServiceStatus.Initializing && maxWait > 0) {
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+		if (maxWait < 1){
+			Debug.Log("Timed out");
+			yield break;
+		}
+		if (service.status == LocationServiceStatus.Failed) {
+			Debug.Log("Unable to determine device location");
+		} else {
+			while(service.status != LocationServiceStatus.Failed){
+				service.Start();
+				Input.gyro.enabled = true;
+				obj.transform.rotation = Quaternion.Euler(Input.gyro.attitude.eulerAngles.x,Input.gyro.attitude.eulerAngles.y,Input.gyro.attitude.eulerAngles.z);
+
+				yield return new WaitForSeconds(1);
+				i++;
+			}
+
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
---
+++ mobile-app/Assets/Script/PhoneCamera.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour {
+	public Text text;
+	public GameObject plane;
+	private bool camAvailable;
+	private WebCamTexture cameraTexture;
+	private Texture defaultBackground;
+
+	public RawImage background;
+	public AspectRatioFitter fit;
+	public bool frontFacing;
+	private int length;
+	// Use this for initialization
+	void Start () {
+		//defaultBackground = background.texture;
+		WebCamDevice[] devices = WebCamTexture.devices;
+		length = devices.Length;
+		if (devices.Length == 0)
+			return;
+
+		for (int i = 0; i < devices.Length; i++)
+		{
+			var curr = devices[i];
+
+			if (curr.isFrontFacing == frontFacing)
+			{
+				cameraTexture = new WebCamTexture(curr.name, Screen.width, Screen.height);
+				break;
+			} else {
+				cameraTexture = new WebCamTexture(curr.name, Screen.width, Screen.height);
+			}
+		}	
+
+		if (cameraTexture == null)
+			return;
+
+		cameraTexture.Play (); // Start the camera
+		plane.GetComponent<Renderer>().material.mainTexture = cameraTexture;
+		//background.texture = cameraTexture; // Set the texture
+
+		camAvailable = true; // Set the camAvailable for future purposes.
+		Screen.autorotateToLandscapeLeft = false;
+		Screen.autorotateToLandscapeRight = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//text.text += "\ndevicesLength:" + length;
+		if (!camAvailable)
+			return;
+		
+		float ratio = (float)cameraTexture.width / (float)cameraTexture.height;
+		fit.aspectRatio = ratio; // Set the aspect ratio
+
+		float scaleY = cameraTexture.videoVerticallyMirrored ? -1f : 1f; // Find if the camera is mirrored or not
+		background.rectTransform.localScale = new Vector3(1f, scaleY, 1f); // Swap the mirrored camera
+
+		int orient = -cameraTexture.videoRotationAngle;
+		background.rectTransform.localEulerAngles = new Vector3(0,0, orient);
+}
+}
